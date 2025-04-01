aedb52c838c0bf78c7da12b2bae8ab9aeb256193
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-04-01 11:09:46 +0530

    fdsihf

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const port = 80
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
 app.use(morgan('dev'));
 app.get('/', (req, res) => {
     
