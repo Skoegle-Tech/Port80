8c204249e6b093182c07fc2ec40f273e22dab636
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-04-01 10:27:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const  express = require('express'); 
+const  cors = require('cors');
+const app = express(); 
+
+const port = 80 
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
