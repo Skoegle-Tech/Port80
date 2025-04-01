fb2bd05ad25f47dae56e9bc4357fd45a4e72c180
Manoj Gowda <84260822+ManojGowda89@users.noreply.github.com>
2025-04-01 10:48:30 +0530

    dsjf

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const  express = require('express'); 
 const  cors = require('cors');
 const app = express(); 
-
+const morgan = require('morgan');
 const port = 80 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+app.use(morgan('dev'));
 app.get('/', (req, res) => {
     
     res.send({query:req?.query})
